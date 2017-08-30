03d1a5b30ea3772b2316ff61051c8fe42a0f6024
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-08-30 11:34:57 +0800

    简单demo

--- src/views/index/indexMain.vue
+++ src/views/index/indexMain.vue
@@ -72,6 +72,19 @@
         </section>
     </section>
 </template>
+<style>
+    .link {
+        stroke: #000;
+        stroke-width: 1.5px;
+    }
+    .node {
+        cursor: move;
+        fill: #ccc;
+        stroke: #000;
+        stroke-width: 1.5px;
+    }
+</style>
+
 <style lang="less" scoped>
     h3{
         height: 30px;
@@ -141,7 +154,8 @@
                 aChart.style.height=winHeight-50-52-30-30+'px';
             },
             drawChart(){
-                var links = [
+                console.dir(d3);
+                /*var links = [
                     {id:1,source: "Microsoft", target: "Amazon", type: "licensing"},
                     {id:2,source: "Microsoft", target: "HTC", type: "licensing"},
                     {id:3,source: "Samsung", target: "Apple", type: "suit"},
@@ -151,17 +165,86 @@
                 ];
                 var nodes = {};
                 links.forEach(function(link) {
-                    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source,id:link.id});
-                    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target,id:link.id});
-                });
-                var w = 960,
-                    h = 500;
+                    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source,id:link.id,type:link.type});
+                    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target,id:link.id,type:link.type});
+                });*/
 
-                console.dir(d3);
+                var graph ={
+                    "nodes": [
+                        {"x": 469, "y": 410},
+                        {"x": 493, "y": 364},
+                        {"x": 442, "y": 365},
+                        {"x": 467, "y": 314},
+                        {"x": 477, "y": 248},
+                        {"x": 425, "y": 207},
+                        {"x": 402, "y": 155},
+                        {"x": 369, "y": 196},
+                        {"x": 350, "y": 148},
+                        {"x": 539, "y": 222},
+                        {"x": 594, "y": 235},
+                        {"x": 582, "y": 185},
+                        {"x": 633, "y": 200}
+                    ],
+                    "links": [
+                        {"source":  0, "target":  1},
+                        {"source":  1, "target":  2},
+                        {"source":  2, "target":  0},
+                        {"source":  1, "target":  3},
+                        {"source":  3, "target":  2},
+                        {"source":  3, "target":  4},
+                        {"source":  4, "target":  5},
+                        {"source":  5, "target":  6},
+                        {"source":  5, "target":  7},
+                        {"source":  6, "target":  7},
+                        {"source":  6, "target":  8},
+                        {"source":  7, "target":  8},
+                        {"source":  9, "target":  4},
+                        {"source":  9, "target": 11},
+                        {"source":  9, "target": 10},
+                        {"source": 10, "target": 11},
+                        {"source": 11, "target": 12},
+                        {"source": 12, "target": 10}
+                    ]
+                };
+                var nodes=graph.nodes;
+                var links=graph.links;
+
+                const w = 960,//后期改为整块区域的宽高，待修改
+                      h = 500;
                 let chartDiv=d3.select('body').select('#chartId');
+                let svg=chartDiv.append("svg")
+                    .attr("width", w)
+                    .attr("height", h);
+                var link = svg.selectAll(".link");
+                var node = svg.selectAll(".node");
                 //引入力导向图
-                let force = d3.forceSimulation();
-                console.log(force);
+                let force = d3.forceSimulation()
+                    .force("charge",d3.forceManyBody())
+                    .force("center",d3.forceCenter(w/2,h/2))
+                    .on("tick",tick)
+                    .nodes(nodes)
+                    .force("link",d3.forceLink(links));
+                
+                link=link.data(links)
+                    .enter().append("line")
+                    .attr("class", "link");
+                node = node.data(nodes)
+                    .enter().append("circle")
+                    .attr("class", "node")
+                    .attr("r", 12)
+                    // .on("dblclick", dblclick)
+                    .call(d3.drag());
+
+                console.log(svg);
+                function tick() {
+                    link.attr("x1", function(d) { return d.source.x; })
+                        .attr("y1", function(d) { return d.source.y; })
+                        .attr("x2", function(d) { return d.target.x; })
+                        .attr("y2", function(d) { return d.target.y; });
+
+                    node.attr("cx", function(d) { d.fx=d.x;return d.x; })
+                        .attr("cy", function(d) { d.fy=d.y;return d.y; });
+                    }
             }
         }
     }
