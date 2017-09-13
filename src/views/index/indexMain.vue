a9fa22ba3102fa182caec95914f9cb6fcf6162a3
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-09-13 14:38:37 +0800

    新增左侧调查概览数据显示及部分交互

--- src/views/index/indexMain.vue
+++ src/views/index/indexMain.vue
@@ -4,8 +4,8 @@
             <h3 class="left">关系图谱调查</h3>
             <div class="right messDiv">
                 <label class="left">查询编号：</label>
-                <input type="text" placeholder="请输入编号" class="left txt">
-                <input type="button" value="查询" class="btnBlue">
+                <input type="text" placeholder="请输入编号" class="left txt" v-model="idNo">
+                <input type="button" value="查询" class="btnBlue" @click="getData">
             </div>
         </div>
         <section class="mainContain">
@@ -13,34 +13,20 @@
                 <div class="everyDiv box-shadow">
                     <h5>调查概览：</h5>
                     <ul>
-                        <li class="messLi">
-                            <span @click="changeShowF">评分-D</span>
-                            <ul v-show="showF==1">
-                                <li>1111</li>
-                                <li>222</li>
+                        <li class="messLi" v-for="(infos,index) in surveyInfos" :key="index">
+                            <span @click="changeShowF(infos.ocMenubm)" class="menuSpan">{{infos.name}} <i>{{infos.sl}}</i></span>
+                            <ul v-show="showF==infos.ocMenubm" v-if="infos">
+                                <li v-for="(ocMenu,curIndex) in infos.children" :key="curIndex">{{ocMenu.name}} <i>{{ocMenu.sl}}</i></li>
+                                <!--<li>222</li>-->
                             </ul>
                         </li>
-                        <li class="messLi">
+                        <!--<li class="messLi">
                             <span>评分-C</span>
                             <ul v-show="showF==2">
                                 <li>1111</li>
                                 <li>222</li>
                             </ul>
-                        </li>
-                        <li class="messLi">
-                            <span>评分-B</span>
-                            <ul v-show="showF==3">
-                                <li>1111</li>
-                                <li>222</li>
-                            </ul>
-                        </li>
-                        <li class="messLi">
-                            <span>评分-A</span>
-                            <ul v-show="showF==4">
-                                <li>1111</li>
-                                <li>222</li>
-                            </ul>
-                        </li>
+                        </li>-->
                     </ul>
                 </div>
                 <div class="everyDiv box-shadow">
@@ -161,6 +147,13 @@
                 padding:5px 10px;
                 box-sizing:border-box;
             }
+            .menuSpan{
+                cursor: pointer;
+            }
+            i{
+                float: right;
+                font-style: normal;
+            }
         }
     }
 </style>
@@ -174,11 +167,13 @@
                 chartHeight:400,
                 graph:null,
                 showF:-1,
+                idNo:'',
+                surveyInfos:null,//调查概览数据
             }
         },
         mounted(){
             this.getChartHeight();
-            this.getData();
+            //this.getData();
         },
         methods:{
             getChartHeight(){
@@ -190,15 +185,15 @@
                 this.chartWidth=aChart.clientWidth;
             },
             getData(){
-                this.$http.get('/graph/data.gm?idNo=341823197004287053').then(function(res){
+                this.$http.get('/graph/data.gm?idNo='+this.idNo).then(function(res){
                     this.graph=res.data.data;
+                    this.surveyInfos=res.data.data.surveyInfos.concat();
                     this.drawChart();//画图
                 })
                 //this.drawChart();//画图
             },
             drawChart(){
-                console.dir(d3);
-                
+                const _this=this;
                 /*this.graph ={
                     "nodes": [ { name: "BeiJing",type:"person"   }, { name: "XiaMen",type:'suit' },
                           { name: "XiAn"    }, { name: "HangZhou"   },
@@ -208,6 +203,12 @@
                            { source : 2  , target: 3,type:'ww' } , { source : 1  , target: 4 ,type:'cc'} ,
                            { source : 1  , target: 5,type:'vv' } , { source : 3  , target: 6 ,type:'22'}  ]
                 }*/
+                //每次请求完重新加载显示图
+                d3.select('#svgId').remove();   //删除整个SVG
+                d3.select('#svgId')
+                    .selectAll('*')
+                    .remove();                    //清空SVG中的内容
+                //开始设置
                 let nodes=this.graph.nodes,
                     links=this.graph.links;
 
@@ -215,6 +216,7 @@
                       h = this.chartHeight;
                 let chartDiv=d3.select('body').select('#chartId');
                 let svg=chartDiv.append("svg")
+                    .attr('id','svgId')
                     .attr("width", w)
                     .attr("height", h);
                 //箭头
@@ -241,7 +243,7 @@
                     .force("center",d3.forceCenter(w/2,h/2))
                     .on("tick",tick)
                     .nodes(nodes)
-                    .force("link",d3.forceLink(links).distance(200)/*.strength(2)*/);//forceLink连接力 distance连接线长度 strength连接强度
+                    .force("link",d3.forceLink(links).distance(150)/*.strength(2)*/);//forceLink连接力 distance连接线长度 strength连接强度
                 
                 link=link.data(links)
                     .enter().append("path")
@@ -286,7 +288,7 @@
                         .on("start",dragstarted)
                         .on("drag",dragged)
                         .on("end",dragended))
-                    .on('click',nodeClick);                                                                                                  
+                    .on('click',_this.nodeClick);                                                                                                  
                 //节点上的文字
                 var svg_texts = svg.selectAll("text")
                     .data(nodes)
@@ -303,7 +305,8 @@
                     .call(d3.drag()
                         .on("start",dragstarted)
                         .on("drag",dragged)
-                        .on("end",dragended));
+                        .on("end",dragended))
+                    .on('click',_this.nodeClick);
                 //线上的文字
                 var line_texts=svg.selectAll(".linetext")  
                     .data(links)  
@@ -322,13 +325,9 @@
                     .text(function(d){  
                         return d.type;  
                     })
-                    .on('click',function(d){
-                        console.log(d.type)
-                    }); 
+                    .on('click',_this.lineClick); 
 
-                function nodeClick(d){
-                    console.log(d.name);
-                }
+
                 function tick() {
                     /*link.attr("x1", function(d) { return d.source.x; })
                         .attr("y1", function(d) { return d.source.y; })
@@ -381,8 +380,30 @@
                     d.fy = null;
                 }
             },
-            changeShowF(){
+            changeShowF(menuId){
+                if(this.showF==menuId){
+                    this.showF=-1;
+                    return;
+                }
+                this.showF=menuId;
+            },
+            nodeClick(d){
+                let nodeId=d.id,
+                    nodeType=d.type;
+                this.$http.get('/graph/findSubjectAtt.gm?id='+nodeId+'&type='+nodeType).then(function(res){
+                    if(res.data.data.code==200){
 
+                    }
+                })
+            },
+            lineClick(d){
+                let linkId=d.id,
+                    linkType=d.type;
+                this.$http.get('/graph/findRelationAtt.gm?id='+linkId+'&type='+linkType).then(function(res){
+                    if(res.data.data.code==200){
+
+                    }
+                })
             }
         }
     }
